d0335de619a8b91cc53331e76db4b1f14df4344d
Shamil Alisultanov <SHOMA.ALISULTANOV@YANDEX.RU>
2018-10-15 08:42:58 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-alert('a')
+var textarea  = document.querySelector('textarea');
+var output = document.querySelector('.result')
+
+textarea.addEventListener('keydown', function() {
+  let text = this.value.split(" ");
+  text = text.map( (i) => {
+    console.log(i);
+    console.log('i[0]', i[0]);
+    if(i[0] == "#") {
+      return `<a href='#'>${i}</a>`;
+    }
+    return  i;
+  });
+  output.innerHTML = text.join(" ");
+});
